46eb39b6feefc2d85c47d6a17fc7ced82a9fe7ae
miguel3142250004-hub <miguel_3142250004@utd.edu.mx>
2026-06-11 17:31:20 -0600

    Add files via upload

---
+++ 2_modulos_paquetes/paquete1/modulo_paquete.py
@@ -0,0 +1,11 @@
+# Un módulo es simplemente un archivo con extensión .py que contiene código de Python (funciones, clases, variables, etc.).
+
+# Un paquete es una carpeta que contiene varios módulos (archivos .py) y un archivo especial llamado __init__.py, que le indica a Python que esa carpeta debe tratarse como un paquete.
+
+def funcion4(nom,ape,ed):
+    mensaje=f"El nombre completo de la persona es: {nom} {ape}"
+    return mensaje
+
+def Edad():
+ edad=int(input("Edad:"))
+ return edad
---
+++ 2_modulos_paquetes/utilizar_modulos.py
@@ -0,0 +1,24 @@
+# 1er utilizar los modulos 
+import modulos
+
+modulos.borrarPantalla()
+modulos.funcion1()
+
+nom="Daniel"
+ape="Carreon"
+
+name,lastname=modulos.funcion4(nom,ape)
+print(f"Nombre:{name}\nApellidos: {lastname}")
+
+
+#2da formar de utilizar modulos
+from modulos import borrarPantalla,funcion1,funcion4
+
+borrarPantalla()
+funcion1()
+
+nom="Daniel"
+ape="Carreon"
+
+name,lastname=funcion4(nom,ape)
+print(f"Nombre:{name}\nApellidos:{lastname}")
